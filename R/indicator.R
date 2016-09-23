112bdf436a24eb82498fe433078a5cdab9563796
Philippe Massicotte <pmassicotte@hotmail.com>
2016-09-23 14:53:51 +0200

    first draft of the indicator function

---
+++ R/indicator.R
@@ -0,0 +1,20 @@
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# AUTHOR:       Philippe Massicotte
+#
+# DESCRIPTION:  Function used to calculate indicators. Only takes 1 parameter
+#               which specify which indicator should be calculated.
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+get_indicator <- function(which = c("cumcover", "fraction", "succession")) {
+
+  # ***************************************************************************
+  # glmm models
+  # 
+  # Find which package to use
+  # 1. Gaussian distribution
+  # 2. Allow fixed parameter value
+  # ***************************************************************************
+  
+  
+  
+}
