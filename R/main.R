31a2506000efaf7f815659493349c87dab48908e
Philippe Massicotte <pmassicotte@hotmail.com>
2016-12-22 09:04:24 -0500

    updated main

--- R/main.R
+++ R/main.R
@@ -2,17 +2,23 @@ library(tidyverse)
 library(haven)
 library(lme4)
 
+library(extrafont)
+loadfonts(quiet = TRUE)
+theme_set(theme_bw(base_size = 12, base_family = "Open Sans"))
+
+# source("R/extract_modis.R") # ATTENTION: takes few minutes
+
 # Clean the workspace
 rm(list = ls())
 
 source("R/read_params.R")
-source("R/indicator.R")
 source("R/process_data.R")
-
-# params <- read_params()
-
-# View(params[[2]])
+source("R/indicator.R")
 
 df <- read_sas("data/alsfjord.sas7bdat")
 
+df <- process_data(df)
+
 indicator <- MacroAlgaeIndicator_CumulativeCover(df)
+
+# write_csv(indicator, "/home/pmassicotte/Desktop/data_ready.csv")
