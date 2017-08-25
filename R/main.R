2a848b070fe8dcf9fdb69f6f92522e85a1890cc5
Jacob Carstensen <au223594@uni.au.dk>
2017-08-25 15:48:45 +0200

    ji

--- R/main.R
+++ R/main.R
@@ -12,8 +12,16 @@ theme_set(theme_bw(base_size = 12, base_family = "Open Sans"))
 rm(list = ls())
 
 source("R/read_params.R")
+source("R/modified_lmer.R")
 source("R/MacroAlgaeIndicator_CumulativeCover.R")
 
 df <- read_sas("data/alsfjord.sas7bdat")
+df <- read_sas("data/alsfjord_2013_2016.sas7bdat")
+df <- read_sas("data/alsfjord_2007_2012.sas7bdat")
+df <- read_sas("data/alsfjord_2001_2006.sas7bdat")
+df <- read_sas("data/dybsoefjord_2001_2006.sas7bdat")
 
-indicator <- MacroAlgaeIndicator_CumulativeCover(df)
+#Examples
+indicator <- MacroAlgaeIndicator("CumulativeCover",df,boundaries=c(74.4,40.8,20.4,13.8),depth_cutoff=1)
+indicator <- MacroAlgaeIndicator("PropOpportunist",df,boundaries=c(0.25,0.40,0.60,0.80),depth_cutoff=1)
+indicator <- MacroAlgaeIndicator("NPerennials",df,boundaries=c(10,8,5,2),depth_cutoff=1)
