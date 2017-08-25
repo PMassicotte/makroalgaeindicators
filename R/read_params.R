37c88c62f2a14e9cf5bc1359db4618535d43a442
Jacob Carstensen <au223594@uni.au.dk>
2017-08-25 15:42:32 +0200

    ho

--- R/read_params.R
+++ R/read_params.R
@@ -1,11 +1,19 @@
 read_params <- function() {
   
-  covparams <- haven::read_sas("data/covparms_cumcover.sas7bdat")
-  parmest <- haven::read_sas("data/parmest_cumcover.sas7bdat")
+  covparams_CumCover <- haven::read_sas("data/covparms_cumcover.sas7bdat")
+  parmest_CumCover <- haven::read_sas("data/parmest_cumcover.sas7bdat")
+  covparams_PropOpportunist <- haven::read_sas("data/covparms_filaandel.sas7bdat")
+  parmest_PropOpportunist <- haven::read_sas("data/parmest_filaandel.sas7bdat")
+  covparams_NPerennials <- haven::read_sas("data/covparms_narter_perennial.sas7bdat")
+  parmest_NPerennials <- haven::read_sas("data/parmest_narter_perennial.sas7bdat")
   
   res <- list(
-    covparams = covparams,
-    parmest = parmest
+    covparams_CumCover = covparams_CumCover,
+    parmest_CumCover = parmest_CumCover,
+    covparams_PropOpportunist = covparams_PropOpportunist,
+    parmest_PropOpportunist = parmest_PropOpportunist,
+    covparams_NPerennials = covparams_NPerennials,
+    parmest_NPerennials = parmest_NPerennials
   )
   
   return(res)
